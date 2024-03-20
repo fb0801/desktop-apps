c5ca26353a904a1dba9d1ea15ddddd9890ee1d78
fb0801 <fbhatti0801@gmail.com>
2024-03-20 15:54:53 +0000

    SignalsAndSlots

--- app-1/main.py
+++ app-1/main.py
@@ -1,51 +1,65 @@
+
+#VERSION1 : Just responding to the button click : syntax
 """
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PySide6.QtWidgets import QApplication, QPushButton
 
-import sys
-app = QApplication(sys.argv)
+#The slot : responds when something happens
+def button_clicked():
+        print("You clicked the button, didn't you!")
 
-window = QMainWindow()
-window.setWindowTitle("Our first MainWindow App!")
+app = QApplication()
+button = QPushButton("Press Me")
 
-button = QPushButton()
-button.setText("Press Me")
+#clicked is a signal of QPushButton. It's emitted when you click
+#  on the button
+#You can wire a slot to the signal using the syntax below : 
+button.clicked.connect(button_clicked)
 
-window.setCentralWidget(button)
-
-window.show() 
-app.exec()
-
-"""
-
-
-#VERSION2 : Setting up a separate class
-"""
-import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton 
-
-class ButtonHolder(QMainWindow):
-    def __init__ (self):
-        super().__init__()
-        self.setWindowTitle("Button Holder App")
-        button = QPushButton("Press Me!")
-
-        #Set up the button as our central widget
-        self.setCentralWidget(button)
-
-
-app = QApplication(sys.argv)
-
-window = ButtonHolder()
-window.show() 
+button.show()
 app.exec()
 """
 
 
-import sys
-from PySide6.QtWidgets import QApplication
-from button_holder import ButtonHolder
-app = QApplication(sys.argv)
 
-window = ButtonHolder()
-window.show() 
+#VERSION2 : SIGNALS SENDING VALUES, CAPTURE VALUES IN SLOTS
+"""
+from PySide6.QtWidgets import QApplication, QPushButton
+
+#The slot : responds when something happens
+def button_clicked(data):
+        print("You clicked the button, didn't you! checked : ",data)
+
+app = QApplication()
+button = QPushButton("Press Me")
+button.setCheckable(True) # Makes the button checkable. It's unchecked by default.
+                        # Further clicks toggle between checked and unchecked states
+
+#clicked is a signal of QPushButton. It's emitted when you click on the button
+#You can wire a slot to the signal using the syntax below : 
+button.clicked.connect(button_clicked)
+
+button.show()
+app.exec()
+
+
+"""
+
+#VERSION3 : CAPTURE VALUE FROM A SLIDER - OTHER EXAMPLE
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QSlider
+
+#The slot : responds when something happens
+def respond_to_slider(data):
+        print("slider moved to : ", data)
+
+app = QApplication()
+slider = QSlider(Qt.Horizontal)
+slider.setMinimum(1)
+slider.setMaximum(100)
+slider.setValue(25)
+
+#You just do the connection. The Qt system takes care of
+#  passing the data from the signal to the slot.
+slider.valueChanged.connect(respond_to_slider)
+slider.show()
 app.exec()
